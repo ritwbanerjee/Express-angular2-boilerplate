c341ad0d9a502b718092d3b82648e0ec13d86915
Banerjee, Ritwik (US - Bengaluru) <ritwbanerjee@deloitte.com>
2017-02-25 05:48:57 +0530

    Split server.js into express.js and server.js

--- server.js
+++ server.js
@@ -1,41 +1,28 @@
+'use strict';
+
 // Get dependencies
 const express = require('express');
-const path = require('path');
 const http = require('http');
-const bodyParser = require('body-parser');
-
-// Get our API routes
-const api = require('./server/routes/api');
+const path = require('path');
 
 const app = express();
-
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Set our api routes
-app.use('/api', api);
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
+require(__dirname + '/server/express')(app);
 
 /**
  * Create HTTP server.
  */
 const server = http.createServer(app);
 
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
 /**
  * Listen on provided port, on all network interfaces.
  */
+const port = app.get('port');
 server.listen(port, () => console.log(`API running on localhost:${port}`));
